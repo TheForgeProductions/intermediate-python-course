2f8be62f5684c6a85ae3836d1e61b8dcdc2bc71e
Mike Brown <theforgesound@gmail.com>
2020-04-08 23:40:04 -0700

    couple conditionals for different results

--- dice_roller.py
+++ dice_roller.py
@@ -3,10 +3,14 @@ import random
 def main():
   dice_rolls = int(input('How many dice would you like to roll?\n'))
   dice_total = 0
-  print(dice_rolls)
   for i in range(1,dice_rolls):
     roll = random.randint(1,6)
-    print(f'You rolled a {roll}')
+    if roll == 2:
+      print('Snake eyes mothafuckah!')
+    elif roll == 6:
+      print('Its a six')      
+    else:
+       print(f'You rolled a {roll}')
     dice_total += roll
   print(f'Total: {dice_total}')
 

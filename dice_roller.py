2a4bbd38da781226f78648e2e717d3d3f6b15449
Mike Brown <theforgesound@gmail.com>
2020-04-08 23:54:21 -0700

    Tutorial complete

--- dice_roller.py
+++ dice_roller.py
@@ -3,8 +3,9 @@ import random
 def main():
   dice_rolls = int(input('How many dice would you like to roll?\n'))
   dice_total = 0
+  dice_size = int(input('How many sides per die?\n'))
   for i in range(1,dice_rolls):
-    roll = random.randint(1,6)
+    roll = random.randint(1,dice_size)
     if roll == 2:
       print('Snake eyes mothafuckah!')
     elif roll == 6:

e3365c02c54b6bd37dffe51240e831459204faad
Mike Brown <theforgesound@gmail.com>
2020-04-08 22:57:40 -0700

    prompts for number of rolls, rolls that many dice, and prints each result plus total

--- dice_roller.py
+++ dice_roller.py
@@ -1,8 +1,14 @@
 import random
 
 def main():
-  roll = random.randint(1,6)
-  print("You rolled a die\nIt's a", roll)
+  dice_rolls = int(input('How many dice would you like to roll?\n'))
+  dice_total = 0
+  print(dice_rolls)
+  for i in range(1,dice_rolls):
+    roll = random.randint(1,6)
+    print(f'You rolled a {roll}')
+    dice_total += roll
+  print(f'Total: {dice_total}')
 
 if __name__== "__main__":
   main()
